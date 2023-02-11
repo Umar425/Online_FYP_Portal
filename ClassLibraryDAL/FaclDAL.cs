6ba9d1a1536145c6fc45576a91744fd8029988c5
MuhammadUbaidZahoor <mirzzahbk1@gmail.com>
2023-02-11 12:56:41 +0500

    Resolved FaclDAL , Razor and  FaclModel

--- ClassLibraryDAL/FaclDAL.cs
+++ ClassLibraryDAL/FaclDAL.cs
@@ -1,68 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlClient;
-using ClassLibraryModel;
-using System.Data.Common;
 using System.Data;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Data.SqlClient;
+using FacultyProject.Models;
 
-namespace ClassLibraryDAL
+namespace FacultyProject.DAL
 {
-	public class FaclDAL
-	{
-		public static int SaveFacl(FaclModel fm)
-		{
-			SqlConnection con = DBHelper.GetConnection();
-			con.Open();
-			SqlCommand cmd = new SqlCommand("Sp_SaveFacl", con);
-			cmd.CommandType = System.Data.CommandType.StoredProcedure;
-			cmd.Parameters.AddWithValue("@FaclFirstName", fm.FaclFirstName);
-			cmd.Parameters.AddWithValue("@FaclLastName", fm.FaclLastName);
-			cmd.Parameters.AddWithValue("@FaclPhoneNo", fm.FaclPhoneNo);
-			cmd.Parameters.AddWithValue("@FaclEmail", fm.FaclEmail);
-			cmd.Parameters.AddWithValue("@FaclAddress", fm.FaclAddress);
-			cmd.Parameters.AddWithValue("@PosID", fm.PosID);
-			cmd.Parameters.AddWithValue("@ExpID", fm.ExpID);
-			cmd.Parameters.AddWithValue("@QualID", fm.QualID);
-			cmd.Parameters.AddWithValue("@DeptID", fm.DeptID);
-			cmd.Parameters.AddWithValue("@OrgID", fm.OrgID);
-			int i = cmd.ExecuteNonQuery();
-			con.Close();
-			return i;
-		}
+    public class FaclDAL
+    {
+        private readonly string connectionString;
 
-		public static List<FaclModel> GetFacl()
-		{
-			SqlConnection con = DBHelper.GetConnection();
-			con.Open();
-			SqlCommand cmd = new SqlCommand("Sp_GetFacl", con);
-			cmd.CommandType = System.Data.CommandType.StoredProcedure;
+        public FaclDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
 
-			SqlDataReader sdr = cmd.ExecuteReader();
-			List<FaclModel> Facllist = new List<FaclModel>();
-			while (sdr.Read())
-			{
+        public int AddProject(ProjectModel facl)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand("INSERT INTO FacultyProject (ProjectName, ProjectDetails, StartDate, EndDate) VALUES (@ProjectName, @ProjectDetails, @StartDate, @EndDate)", connection);
 
-				FaclModel facl = new FaclModel();
-				facl.FaclID = int.Parse(sdr["FaclID"].ToString());
-				facl.FaclFirstName = sdr["FaclFirstName"].ToString();
-				facl.FaclLastName = sdr["FaclLastName"].ToString();
-				facl.FaclPhoneNo = sdr["FaclPhoneNo"].ToString();
-				facl.FaclEmail = sdr["FaclEmail"].ToString();
-				facl.FaclAddress = sdr["FaclAddress"].ToString();
-				facl.PosID = int.Parse(sdr["PosID"].ToString());
-				facl.ExpID = int.Parse(sdr["ExpID"].ToString());
-				facl.QualID = int.Parse(sdr["QualID"].ToString());
-				facl.DeptID = int.Parse(sdr["DeptID"].ToString());
-				facl.OrgID = int.Parse(sdr["OrgID"].ToString());
-				Facllist.Add(facl);
-			}
+                cmd.Parameters.AddWithValue("@ProjectName", facl.ProjectName);
+                cmd.Parameters.AddWithValue("@ProjectDetails", facl.ProjectDetails);
+                cmd.Parameters.AddWithValue("@StartDate", facl.StartDate);
+                cmd.Parameters.AddWithValue("@EndDate", facl.EndDate);
 
-			con.Close();
-			return Facllist;
-		}
-	}
+                connection.Open();
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int DeleteProject(int faclID)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand("DELETE FROM FacultyProject WHERE ProjectID = @ProjectID", connection);
+                cmd.Parameters.AddWithValue("@ProjectID", faclID);
+
+                connection.Open();
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public DataTable GetProjects()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand("SELECT * FROM FacultyProject", connection);
+                var adapter = new SqlDataAdapter(cmd);
+                var table = new DataTable();
+
+                connection.Open();
+                adapter.Fill(table);
+
+                return table;
+            }
+        }
+    }
 }
+
---
+++ ClassLibraryModel/Connectionstring.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryModel
+{
+    internal class Connectionstring
+    {
+        public class DBConnection
+        {
+            private static string connectionString = @"Data Source=DESKTOP-JSIV0OT\MSSQLSERVER01;Initial Catalog=YourDB;Integrated Security=True";
+
+            public static string GetConnectionString()
+            {
+                return connectionString;
+            }
+        }
+
+    }
+}
